1b9637fa1e840d823be8ecf6507edd6855f4c389
mileticveljko <mileticveljko99@gmail.com>
2024-11-14 14:56:36 +0100

    move lazy.lua file content init.lua

--- init.lua
+++ init.lua
@@ -1,11 +1,25 @@
 -- Nvim version 0.9.5
+local user = "user"
 
-User = "user"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-local options = User .. ".options"
-local keymaps = User .. ".keymaps"
-local lazy = User .. ".lazy"
+local status_ok, lazy = pcall(require, "lazy")
+if not status_ok then
+  print("Couldn't load Packer!")
+	return
+end
 
-pcall(require, options)
-pcall(require, keymaps)
-pcall(require, lazy)
+pcall(require, user .. ".options")
+pcall(require, user .. ".keymaps")
+lazy.setup(user .. ".plugins")
